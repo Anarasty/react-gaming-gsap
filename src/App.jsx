2d496ae1bc8866e981608f6aa402694217fd9cc0
Anarasty <murloc.ninja16@gmail.com>
2026-06-07 16:26:39 +0200

    aded features

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import About from "./components/About";
+import Features from "./components/Features";
 import Hero from "./components/Hero";
 import NavBar from "./components/Navbar";
 
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <NavBar/>
+      <NavBar />
       <Hero />
       <About />
+      <Features />
     </main>
   );
 }
